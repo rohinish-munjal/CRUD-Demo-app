ed6e7b0b1cfa815034de85536a5c33d2570be83d
rohinish-munjal <rohinish.munjal@forwardai.com>
2022-07-04 11:17:05 +0530

    initial commit

---
+++ src/models/user.model.ts
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import config from "config";
+
+export interface UserInput {
+  email: string;
+  name: string;
+  password: string;
+}
+
+export interface UserDocument extends UserInput, mongoose.Document {
+  createdAt: Date;
+  updatedAt: Date;
+  comparePassword(candidatePassword: string): Promise<Boolean>;
+}
+
+const userSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true,  //give created and updated timestamps
+  }
+);
+ 
+//For hashing the password
+userSchema.pre("save", async function (next) {
+  let user = this as UserDocument;
+//If password is not modified it will return next
+  if (!user.isModified("password")) {
+    return next();
+  }
+//the event loop sees it as a sequence of functions, invoked over time. Salt provides a simple pattern to describe those sequences, along with an API to control timing.
+  const salt = await bcrypt.genSalt(config.get<number>("saltWorkFactor"));
+
+  const hash = await bcrypt.hashSync(user.password, salt);
+
+  user.password = hash;
+
+  return next();
+});
+
+userSchema.methods.comparePassword = async function (
+  candidatePassword: string
+): Promise<boolean> {
+  const user = this as UserDocument;
+
+  return bcrypt.compare(candidatePassword, user.password).catch((e) => false);
+};
+
+const UserModel = mongoose.model<UserDocument>("User", userSchema);
+//By using UserDocument we have passed our own interface, giving type definitions like the custom properties on the Schema
+
+export default UserModel;
